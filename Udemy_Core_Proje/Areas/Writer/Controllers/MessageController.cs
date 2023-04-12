de71783c323fa9594083fd55386b8626207d7aca
Yunus Kalyoncu <95497265+YunusKalyoncu@users.noreply.github.com>
2023-04-12 12:04:17 +0300

    12.04.2023

--- Udemy_Core_Proje/Areas/Writer/Controllers/MessageController.cs
+++ Udemy_Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,7 @@ namespace Udemy_Core_Proje.Areas.Writer.Controllers
     [Area("Writer")]
     public class MessageController : Controller
     {
+        WriterMessageManager writerMessage = new WriterMessageManager(new EfWriterMessageDal()); 
         public IActionResult Index()
         {
             return View();
